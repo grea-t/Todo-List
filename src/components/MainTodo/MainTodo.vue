1344e089914d4266ee8eb0f41e13aa642cf0fb65
grea-t <1193976273@qq.com>
2020-11-03 14:44:16 +0800

    MainTodo样式

--- src/components/MainTodo/MainTodo.vue
+++ src/components/MainTodo/MainTodo.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <input type="text"/>
+  <div class="main-todo">
+    <input type="text" class="add-todo" placeholder="What to do?" autofocus/>
   </div>
 </template>
 
@@ -10,6 +10,21 @@
   }
 </script>
 
-<style scoped>
+<style lang="stylus" scoped>
+  .main-todo
+    margin 0 auto
+    width 600px
+    background-color: #fff
+    box-shadow 0 0 5px #666
 
+    .add-todo
+      padding 16px 16px 16px 36px
+      width 100%
+      font-size 24px
+      font-family inherit
+      font-weight inherit
+      color inherit
+      border-bottom none
+      outline none
+      box-sizing border-box
 </style>
