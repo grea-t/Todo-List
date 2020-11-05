9b71fd5f3e2abe88f81e6c3558fdda8d37286e40
grea-t <1193976273@qq.com>
2020-11-05 16:34:41 +0800

    简单添加Footer

---
+++ src/components/MainFooter.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer>Written By Grea-t</footer>
+</template>
+
+<style lang="stylus" scoped>
+  footer
+    margin-top 20px
+    text-align center
+    color #fff
+    text-shadow 5px 5px 5px #000
+</style>
